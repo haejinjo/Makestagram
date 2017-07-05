dad9eff569c1a7744996eb9f6b07956e89b9724a
Haejin <haejinjo@g.ucla.edu>
2017-07-05 11:31:00 -0700

    Added logic to AppDelegate class to change our app UIWindow's root view controller

---
+++ Makestagram/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoginViewController.swift
+//  Makestagram
+//
+//  Created by Haejin Jo on 7/5/17.
+//  Copyright © 2017 Haejin Jo. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Do any additional setup after loading the view
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        //Dispose of any resousrces that can be recreated
+    }
+    
+    /*
+    // MARK: - Navigation
+    
+    //In a storyboard-based application, you will often want to do al ittle preparation before navigation
+    override func prepare((for segue: UIStoryboardSegue, sender: Any?) {
+        //Get the new view controller using segue.destinationViewController.
+        //Pass the selected object to the new view controller.
+    }
+    */
+}
